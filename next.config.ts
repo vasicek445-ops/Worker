f3953c06e611a0b5963d1700ff51423a0848a317
Vaclav <vasicek445@email.cz>
2026-02-25 19:55:37 +0100

    New landing page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      { source: '/', destination: '/landing.html' },
+    ];
+  },
 };
-
 export default nextConfig;
